74a2cda0958e7a98ef05431ca25e442f7404f9a0
Fadie Hannona <fadiehannona@gmail.com>
2014-10-22 14:18:34 +0100

    temperatures added

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -1,3 +1,11 @@
 describe ('Thermostat', function () {
 
+	beforeEach(function(){
+		thermostat = new Thermostat
+	});
+
+it ('it will have a range of temperatures', function(){
+	expect(thermostat.temperature.length).toEqual(13)
+})
+
 });
---
+++ src/thermostat.js
@@ -0,0 +1,5 @@
+function Thermostat() {
+
+	this.temperature = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+
+};
