d2817485ac787d403ed50822775e207a9acd694b
Fadie Hannona <fadiehannona@gmail.com>
2014-10-22 14:38:23 +0100

    can increase the temperature by 1

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -5,11 +5,17 @@ describe ('Thermostat', function () {
 	});
 
 it ('it will have a range of temperatures', function(){
-	expect(thermostat.temperature.length).toEqual(13)
+	expect(thermostat.temperatureRange.length).toEqual(13)
 });
 
 it ('should be 20 degrees at the start', function(){
 	expect(thermostat.startTemperature).toEqual(20)
+});
+
+it ('can increase the temperature', function(){
+	expect(thermostat.temperature).toEqual(20)
+	thermostat.increaseTemp(1)
+	expect(thermostat.temperature).toEqual(21)
 });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,10 @@
 function Thermostat() {
 
-	this.temperature = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+	this.temperature = 
 	this.startTemperature = 20
+	this.temperatureRange = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+};
+
+Thermostat.prototype.increaseTemp = function() {
+	this.temperature = 21
 };
